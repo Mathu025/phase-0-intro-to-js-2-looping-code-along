8b4421ba7f443460111f21d9fe2cb6023d5e492f
Mathu025 <mathualex72@gmail.com>
2025-06-16 18:32:33 +0300

    Complete Looping Lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+        debugger;
+    }
+    return gifts;
+}
+wrapGifts(gifts);
+
+function writeCards(names, eventName) {
+    const messages= [];
+    for (let i = 0; i < names.length; i++) {
+        const message = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`
+        messages.push(message);}
+        return messages;
+    }
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number)
+        number--;
+    }
+
+}
